2b0c66499e9297a74bcfac47310427f23ed5e986
lamve2 <venice.lam@oocl.com>
2018-11-21 16:43:42 +0800

    finish map_to_even

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,18 @@
 'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+function times2(element){
+  return element * 2;
 }
+function doMapping(collection, fn){
+  for (var i = 0; i < collection.length; i++){
+    collection[i] = fn(collection[i]);
+  }
+  return collection;
+}
+
+
+function map_to_even(result){
+ return doMapping(result, times2);
+}
+
+
 module.exports = map_to_even;
